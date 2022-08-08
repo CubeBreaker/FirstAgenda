1c22eafc60b51895c4d538486916030b8be398ae
SLCube <kgm0096@naver.com>
2022-08-08 21:15:34 +0900

    백엔드 프로젝트 생성 및 라및 라라이브러리 설정 추가

---
+++ cbchat/src/main/java/com/cbchat/cbchat/CbchatApplication.java
@@ -0,0 +1,13 @@
+package com.cbchat.cbchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbchatApplication.class, args);
+	}
+
+}
